1d9933e56163ff1909b290c07e2e8f67c58f0927
Marcos Silva Cunha <marcos21xx@gmail.com>
2020-10-19 01:09:10 -0300

    Atualizado Elasticsearch para 7.9.2 e melhorias menores

--- yamls/elasticsearch-cluster/deploy.sh
+++ yamls/elasticsearch-cluster/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-kubectl apply -f namespace-elasticsearch.yaml 
+kubectl apply -f elasticsearch-namespace.yaml 
 kubectl apply -f elasticsearch-master.yaml -n nm-elasticsearch
 kubectl apply -f elasticsearch-workers.yaml -n nm-elasticsearch
+kubectl apply -f kibana.yaml -n nm-elasticsearch
+kubectl apply -f lmenezes-cerebro.yaml -n nm-elasticsearch
 
--- yamls/elasticsearch-cluster/wipeout.sh
+++ yamls/elasticsearch-cluster/wipeout.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+kubectl delete -f lmenezes-cerebro.yaml -n nm-elasticsearch
+kubectl delete -f kibana.yaml -n nm-elasticsearch
 kubectl delete -f elasticsearch-workers.yaml -n nm-elasticsearch
 kubectl delete -f elasticsearch-master.yaml -n nm-elasticsearch
-kubectl delete -f namespace-elasticsearch.yaml 
+kubectl delete -f elasticsearch-namespace.yaml 
 
